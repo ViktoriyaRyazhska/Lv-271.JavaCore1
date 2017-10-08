9f5a9041e09454e8d9f925aa2bfe31b52bc480e0
Vitalii <vitalii@mail.com>
2017-10-09 00:29:32 +0300

    add HW3

---
+++ HomeWork3/Person.java
@@ -0,0 +1,58 @@
+package HomeWork3;
+
+public class Person {
+	private String name;
+	private int birthYear;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getBirthYear() {
+		return birthYear;
+	}
+	public void setBirthYear(int birthYear) {
+		this.birthYear = birthYear;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", birthYear=" + birthYear + "]";
+	}
+	public Person() {}
+	public Person(String name, int birthYear) {
+		this.name = name;
+		this.birthYear = birthYear;
+	}
+	
+	public int age() {
+		int a = 2017-birthYear;
+		return a;
+	}
+	public void changeName(String newName) {
+		this.name = newName;
+	
+	}
+	
+	
+	public static void main (String[] args) {
+		Person p1 = new Person("Vasya",1993);
+		Person p2 = new Person("Vitalii",1996);
+		Person p3 = new Person("Misha",1884);
+		Person p4 = new Person("Sara", 1987);
+		Person p5 = new Person("Leonardo", 1889);
+		
+		System.out.println(p1.name + " is " + p1.age() + " years old.");
+		System.out.println(p2.name + " is " + p2.age() + " years old.");
+		System.out.println(p3.name + " is " + p3.age() + " years old.");
+		System.out.println(p4.name + " is " + p4.age() + " years old.");
+		System.out.println(p5.name + " is " + p5.age() + " years old.");
+		System.out.println("");
+		p2.changeName("John");
+		System.out.println(p2);
+		System.out.println(p2.name + " is " + p2.age() + " years old.");
+		
+	}
+}
