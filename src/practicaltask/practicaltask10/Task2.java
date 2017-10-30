4445a0daccb12ee04565ab839015037b43f38d79
Oleksandr Skrypets <blizerod@gmail.com>
2017-10-30 18:17:01 +0200

    add 3th task for practical task 10

--- src/practicaltask/practicaltask10/Task2.java
+++ src/practicaltask/practicaltask10/Task2.java
@@ -4,12 +4,17 @@ public class Task2 {
 	public static void main(String[] args) {
 		MyThread mt = new MyThread();
 		Thread first = new Thread(mt, "MyThead");
-		first.start();
-		
 		MyThread2 mt2 = new MyThread2();
 		Thread second = new Thread(mt2, "MyThread2");
 		second.start();
-		
+		first.start();
+		try {
+			first.join();
+			second.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("My name is Jhon");
 	}
 }
--- src/practicaltask/practicaltask10/WorkFile.java
+++ src/practicaltask/practicaltask10/WorkFile.java
@@ -6,33 +6,67 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WorkFile {
 
 	public static void main(String[] args) {
 		ArrayList<String> arrStr = new ArrayList<>();
+		FileInputStream fstream;
+		BufferedReader br;
+		String str = null;
 		try {
-			FileInputStream fstream = new FileInputStream("D:/file.txt");
-			BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
-			int ch;
-			String str;
-
+			fstream = new FileInputStream("mytext.txt");
+			br = new BufferedReader(new InputStreamReader(fstream));
 			try {
-				while ((ch = br.read()) != -1){
-				    str = String.valueOf((char)ch); 
+				while ((str = br.readLine()) != null) {
 					arrStr.add(str);
-				   }
+				}
+
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		for (String item: arrStr) {
-			System.out.println(item);
-		}
-		System.out.println(arrStr.size());
+		// for (String item : arrStr) {
+		// System.out.println(item);
+		// }
+		countLine(arrStr);
+		check(arrStr, "Трамп");
 	}
 
+	public static void countLine(ArrayList<String> arrStr) {
+		int min = arrStr.get(0).length();
+		int max = 0;
+		int indexMin = 0;
+		int indexMax = 0;
+		for (int i = 0; i < arrStr.size(); i++) {
+			System.out.println(i + "th string has " + arrStr.get(i).length() + "symbols");
+			if (arrStr.get(i).length() < min) {
+				min = arrStr.get(i).length();
+				indexMin = i;
+			}
+			if (arrStr.get(i).length() > max) {
+				max = arrStr.get(i).length();
+				indexMax = i;
+			}
+		}
+		System.out.println("The most longest string is " + indexMax + "th and value - " + max);
+		System.out.println("The most shortest string is " + indexMin + "th and value - " + min);
+	}
+
+	public static void check(ArrayList<String> arrStr, String str) {
+		String temp;
+		for (int i = 0; i < arrStr.size(); i++) {
+		temp = arrStr.get(i);
+			if (temp.contains(str)) {
+		System.out.println("String #" + i + "have word " + str);
+		}
+		
+						
+		}
+
+	}
 }
